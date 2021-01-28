de8c7e9691778733d331709d162928387d006bcf
HarryBrown01 <HBrown@qa.com>
2021-01-28 15:26:48 +0000

    Initial Commit

--- src/PeerPackage/Factorials.java
+++ src/PeerPackage/Factorials.java
@@ -1,5 +1,21 @@
 package PeerPackage;
 
 public class Factorials {
-
+	public static void main(String[] args) {
+		double factNumber = 120;
+		int divStart = 1;
+		
+		while(factNumber>1) {
+			if ((int) factNumber == factNumber) {
+				factNumber /= divStart;
+				if (factNumber > 1) {
+					divStart ++;
+				}
+			} else {
+				System.out.println("None");
+				return;
+			}
+		}
+		System.out.println(divStart);
+	}
 }
